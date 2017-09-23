0458835b7179278c514e0036fe3ff0c7d5f4f237
ethaan <ethan.rosanoo@gmail.com>
2017-09-23 11:46:01 -0500

    Better use of crontask for lists

--- imports/api/tasks/lists.js
+++ imports/api/tasks/lists.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Bots } from '/imports/api/bots/Bots';
 import { Channels } from '/imports/api/bots/Channels';
 import { createCron } from '/imports/api/tasks/config';
@@ -8,6 +9,15 @@ import { updateTemspeakChannel } from '/imports/api/teamSpeak/methods';
 import { mediviaGetPlayersOnline } from '/imports/api/medivia/methods';
 import { sortByProfessions, buildCharacterDescription } from '/imports/api/teamSpeak/channels-utils';
 
+const updateBot = ({ botId: _id, hasError, message }) => (
+  Bots.update({ _id }, {
+    $set: {
+      error: hasError,
+      errorMessage: message,
+    },
+  })
+);
+
 const PERIOD = 'every 10 seconds';
 
 const methodByName = {
@@ -15,43 +25,53 @@ const methodByName = {
   medivia: mediviaGetPlayersOnline,
 };
 
+const defaultErrorMsg = 'Something went wrong with your bot, please check the data you passed';
+
 const updateChannelDescriptionByList = async ({
   cid,
   botId,
   listId,
 }) => {
-  const bot = Bots.findOne({ _id: botId });
-  const items = ListItems.find({ listId }).fetch();
+  try {
+    const bot = Bots.findOne({ _id: botId });
+    const items = ListItems.find({ listId }).fetch();
 
-  const { world, server } = bot;
+    const { world, server } = bot;
 
-  const playersOnline = await methodByName[server].call({ world });
+    const playersOnline = await methodByName[server].call({ world });
 
-  const itemsCounts = items.length;
+    const itemsCounts = items.length;
 
-  const channelData = { cid };
+    const channelData = { cid };
 
-  const onlinePlayersFromList = sortByProfessions({
-    items: filterLists(playersOnline, items, 'name', 'name'),
-  });
+    const onlinePlayersFromList = sortByProfessions({
+      items: filterLists(playersOnline, items, 'name', 'name'),
+    });
 
-  const onlinePlayersFromListCount = onlinePlayersFromList.length;
+    const onlinePlayersFromListCount = onlinePlayersFromList.length;
 
-  let description = `Online ${onlinePlayersFromListCount}/${itemsCounts} \n \n`;
+    let description = `Online ${onlinePlayersFromListCount}/${itemsCounts} \n \n`;
 
-  onlinePlayersFromList.forEach((item) => {
-    description += buildCharacterDescription({ item });
-    return description;
-  });
+    onlinePlayersFromList.forEach((item) => {
+      description += buildCharacterDescription({ item });
+      return description;
+    });
 
-  channelData.channel_description = description;
+    channelData.channel_description = description;
 
-  const channelUpdate = await updateTemspeakChannel.call({
-    botId,
-    channelData,
-  });
+    const channelUpdate = await updateTemspeakChannel.call({
+      botId,
+      channelData,
+    });
 
-  return channelUpdate;
+    updateBot({ botId, hasError: false, message: '' });
+
+    return channelUpdate;
+  } catch (error) {
+    const errorToPass = error.message || defaultErrorMsg;
+    updateBot({ botId, hasError: true, message: errorToPass });
+    throw new Meteor.Error('ERROR!', errorToPass);
+  }
 };
 
 export default () => (
@@ -64,12 +84,15 @@ export default () => (
     channelName,
   }) => {
     createCron({
-      job: (() => updateChannelDescriptionByList({
-        cid,
-        botId,
-        listId,
-        channelName,
-      })),
+      job: (async () => {
+        const description = await updateChannelDescriptionByList({
+          cid,
+          botId,
+          listId,
+          channelName,
+        });
+        return description;
+      }),
       name: channelName,
       period: PERIOD,
     });
