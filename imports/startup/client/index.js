d0f69bbf627cb4052f12d68369004bbf2cd9d8bd
ethaan <ethan.rosanoo@gmail.com>
2017-10-29 21:17:31 -0500

    Renaming routes file

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
-import Routes from '/imports/startup/client/Routes';
+import Routes from '/imports/startup/client/routes';
 
 Meteor.startup(() => {
   render(<Routes />, document.getElementById('render-target'));
